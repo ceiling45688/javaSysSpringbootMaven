98ab1152552f4bf065b177667b743583af02a119
hut1 <hut1@tcd.ie>
2023-08-19 12:59:38 +0100

    Initial commit

---
+++ src/main/java/com/ceiling45688/helloworld/Application.java
@@ -0,0 +1,15 @@
+package com.ceiling45688.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hu
+ */
+
+@SpringBootApplication
+public class Application {
+     public static void main(String[] args){
+         SpringApplication.run(com.ceiling45688.helloworld.Application.class, args);
+     }
+}
---
+++ src/test/java/com/ceiling45688/helloworld/ApplicationTest.java
@@ -0,0 +1,14 @@
+package com.ceiling45688.helloworld;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTest {
+
+    @Test
+    public void test() {
+        System.out.println("Spring Boot Test!");
+    }
+}
+
