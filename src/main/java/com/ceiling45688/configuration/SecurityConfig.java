1cc728efbc70309aa1b83d3effd9bb1cce5af2d1
hut1 <hut1@tcd.ie>
2023-11-02 23:54:53 +0000

    2

--- src/main/java/com/ceiling45688/configuration/SecurityConfig.java
+++ src/main/java/com/ceiling45688/configuration/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.ceiling45688.filter.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -31,10 +32,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .csrf().disable() // 禁用csrf保护，安全性后续再加强吧，不然每个修改数据请求都要添加令牌太麻烦，但其实这里是有跨域危险的
                 .authorizeRequests()
-                .antMatchers("/users/signup", "/users/login", "/home").permitAll() // 允许所有人访问
-                .antMatchers("/api/admin/**").hasRole("ADMIN") // 只有ADMIN角色的用户可以访问
-                .antMatchers("/api/user/**").hasRole("CUSTOMER") // 只有CUSTOMER角色的用户可以访问
-                .antMatchers("/service-requests/**").hasAnyRole("ADMIN", "MAINTENANCE") // 只有ADMIN和MAINTENANCE角色的用户可以访问
+                    .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                    .antMatchers("/users/signup", "/users/login", "/home").permitAll() // 允许所有人访问
+                    .antMatchers("/api/admin/**").hasRole("ADMIN") // 只有ADMIN角色的用户可以访问
+                    .antMatchers("/api/user/**").hasRole("CUSTOMER") // 只有CUSTOMER角色的用户可以访问
+                    .antMatchers("/service-requests/**").hasAnyRole("ADMIN", "MAINTENANCE") // 只有ADMIN和MAINTENANCE角色的用户可以访问
+                    .antMatchers("/rooms/available").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
@@ -54,11 +57,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     // 自定义cors配置，配置Spring Security的CORS支持，独立于SpringMVC，确保security不会组织跨域请求
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
-        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        source.registerCorsConfiguration("/**", config); // 将cors配置注册到所有路径
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("*")); // 允许所有的头部
+        configuration.setAllowCredentials(true); // 允许证书，如cookies等
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
         return source;
     }
 
